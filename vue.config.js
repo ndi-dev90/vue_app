e892530b457700265e7045e49b2d32c304684a30
ndi-dev90 <ndi.dev90@gmail.com>
2022-06-21 21:45:40 +0200

    commit on 'main'

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require("path");
+
+module.exports = {
+  outputDir: path.resolve(__dirname, "./docs/"),
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue_app/'
+    : '/'
+}
